ba2c7a77022082f2fd51b2e6621fa2d0bf8702c9
mooncutter <102485668+mooncutter@users.noreply.github.com>
2025-05-27 17:44:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,37 @@
-// Odotetaan, että dokumentti on ladattu ennen kuin ajetaan mitään
 $(document).ready(function() {
-
-    // Haetaan tarvittavat DOM-elementit jQueryn avulla
     const $form = $('#todo-form');
     const $input = $('#todo-input');
     const $todoList = $('#todo-list');
 
-    // Ladataan tallennetut tehtävät localStoragesta, tai luodaan tyhjä lista
     let todos = JSON.parse(localStorage.getItem('todos')) || [];
 
-    // Tallennetaan tehtävät localStorageen
     function saveTodos() {
         localStorage.setItem('todos', JSON.stringify(todos));
     }
 
-    // Piirretään kaikki tehtävät listaan
     function renderTodos() {
-        $todoList.empty(); // Tyhjennetään lista ensin
-
+        $todoList.empty();
         todos.forEach((todo, index) => {
-            // Luodaan <li>-elementti jQueryllä ja piilotetaan aluksi
             const $li = $('<li></li>')
                 .toggleClass('completed', todo.completed)
-                .hide(); // Piilotetaan ennen fadeIn
+                .hide();
 
-            // Tehtävän teksti
             const $span = $('<span></span>').text(todo.text);
-
-            // Poista-nappi, jossa data-index
-            const $deleteBtn = $('<button class="delete-btn">Poista</button>')
-                .attr('data-index', index);
-
-            // Checkbox tehtävän suorittamiseen
+            const $deleteBtn = $('<button class="delete-btn">Poista</button>').attr('data-index', index);
             const $checkbox = $('<input type="checkbox" class="complete-btn">')
                 .attr('data-index', index)
                 .prop('checked', todo.completed);
 
-            // Lisätään elementit <li>-elementtiin
             $li.append($span, $deleteBtn, $checkbox);
-
-            // Lisätään <li> listaan ja näytetään fadeIn-efektillä
             $todoList.append($li);
             $li.fadeIn(400);
         });
     }
 
-    // Lomakkeen lähetys: uuden tehtävän lisäys
     $form.on('submit', function(event) {
-        event.preventDefault(); // Estetään lomakkeen oletustoiminta
-
+        event.preventDefault();
         const todoText = $input.val().trim();
 
-        // Validointi: ei tyhjää ja vähintään 3 merkkiä
         if (todoText === '') {
             $input.addClass('error');
             alert('Tehtävä ei voi olla tyhjä!');
@@ -62,46 +42,35 @@ $(document).ready(function() {
             return;
         }
 
-        // Poistetaan virheluokka
         $input.removeClass('error');
 
-        // Luodaan uusi tehtäväobjekti ja lisätään listaan
         todos.push({
             text: todoText,
             completed: false
         });
 
-        // Tallennetaan ja päivitetään näkymä
         saveTodos();
         renderTodos();
 
-        // Tyhjennetään syötekenttä
         $input.val('');
     });
 
-    // Delegoitu tapahtumankäsittely Poista-napille
     $todoList.on('click', '.delete-btn', function() {
         const index = $(this).data('index');
 
-        // Poistetaan tehtävä fadeOut-efektillä
         $(this).closest('li').fadeOut(400, function() {
-            todos.splice(index, 1); // Poistetaan listasta
+            todos.splice(index, 1);
             saveTodos();
-            renderTodos(); // Päivitetään näkymä
+            renderTodos();
         });
     });
 
-    // Tehtävän merkitseminen suoritetuksi / suorittamattomaksi
     $todoList.on('change', '.complete-btn', function() {
         const index = $(this).data('index');
-        todos[index].completed = $(this).prop('checked'); // Päivitetään tila
+        todos[index].completed = $(this).prop('checked');
         saveTodos();
-        renderTodos(); // Päivitetään näkymä (päivittää ulkoasun)
+        renderTodos();
     });
-
-    // Ladataan tehtävät sivun alussa
-    renderTodos();
-});
 
     renderTodos();
 });
